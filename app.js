9cbc255d9165f268590e5d341a925285b1038fd0
Sahithi Yamasani <sahithiyamasani4@gmail.com>
2023-11-01 11:09:26 -0500

    added rows and columns

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var monitorsRouter = require('./routes/monitor');
+var boardRouter = require('./routes/board');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/monitor', monitorsRouter);
+app.use('/board', boardRouter);
+
+/*
+app.get('/board', (req, res) => {
+  let query = req.query;
+  console.log(`rows: ${query.rows}`);
+  console.log(`cols: ${query.cols}`);
+  res.render('board', { title: 'Board Display', query: query });
+});
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,5 +49,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
